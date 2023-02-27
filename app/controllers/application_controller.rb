81389306770a131eaa171af7cca0fcb041b69a64
Code-X <andrewomosh44@gmail.com>
2023-02-27 12:47:25 +0300

    add project & user models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,44 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  # To enable cross origin requests for all routes:
+  set :bind, '0.0.0.0'
+  configure do
+    enable :cross_origin
+  end
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS, PUT, DELETE"
+  end
+  
+  # routes...
+  options "*" do
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
+  end
+
+
+  
+  # Add your ro utes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
+  end
+
+  get "/projects" do
+    projects = Project.all
+    project.to_json
+  end
+
+  get "/project/:id" do
+    project = Project.find(params[:id])
+    project.to_json(only: [:name, :topic, :description, :uploaded_file])
+  end
+
+  get "/users" do
+    "List all users"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+# Make 10 users
+10.times do
+    User.create(
+        name: Faker::Name.name
+    )
+  end
+
+  # Make 10 users
+10.times do
+    Project.create(
+        name: Faker::Team.state,
+        topic: Faker::Space.galaxy,
+        description: Faker::Lorem.paragraphs(number: 1, supplemental: true)
+    )
+  end
 
 puts "✅ Done seeding!"
