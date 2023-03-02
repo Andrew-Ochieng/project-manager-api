82149d28c0838746601644712a6a9ecb0d02215b
Vincent <makokhavomondi@gmail.com>
2023-03-02 13:19:29 +0300

    Modify seed file to use the many-to-many relationship between users and projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ user = User.create(
 
 
 users = []
+users << user
 5.times do
   new_user = User.create(
     username: Faker::Internet.username,
@@ -20,26 +21,22 @@ users = []
 end
 
 puts "2. Create projects"
-5.times do
-  Project.create(
-    name: Faker::Team.state,
-    topic: Faker::Space.galaxy,
-    details: Faker::Lorem.paragraph,
-    user_id: user.id
-  )
-end
-
 projects = []
-20.times do
+30.times do
   projects << Project.create(
     name: Faker::Team.state,
     topic: Faker::Space.galaxy,
     details: Faker::Lorem.paragraph,
-    user_id: users.sample.id
   )
 end
 
-puts "3. Create Statuses"
+puts "3. Associate certain projects with certain users"
+projects.each do |project|
+  ProjectMembership.create(user_id: users.sample.id, project_id: projects.sample.id)
+end
+
+
+puts "4. Create Statuses"
 50.times do 
   Status.create(
     summary: Faker::Lorem.sentence(word_count: rand(1..5)),
